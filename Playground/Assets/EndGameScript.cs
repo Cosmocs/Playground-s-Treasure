e10fd485c8e94b53ca64c97cc6346abd1091ff06
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2022-10-17 21:39:58 -0400

    IF THIS DOESN'T WORK I SWEAR TO GOD

---
+++ Playground/Assets/EndGameScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+
+public class EndGameScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public GameObject[] gone;
+    public GameObject[] Elim;
+    public bool Erased()
+    {
+
+        //checks to see if all squares are gone
+        //look for objects in array
+        for (int i = 0; i < gone.Length; i++)
+        {
+            //check if object is in scene 
+            if (gone[i] != null)
+            {
+                return false;
+
+
+            }
+
+        }
+
+        return true;
+
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "worm")
+        {
+            Elim = GameObject.FindGameObjectsWithTag("Round5");
+            foreach (GameObject elim in Elim) //destroy each varible in Elim
+            {
+                Destroy(elim);
+            }
+
+            Destroy(this.gameObject);
+
+
+            if (Erased())
+            {
+                SceneManager.LoadScene("End");
+
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
+
--- Playground/Assets/RemovalScript.cs
+++ Playground/Assets/RemovalScript.cs
@@ -7,28 +7,7 @@ public class RemovalScript : MonoBehaviour
 {
     private Rigidbody2D rb;
     public GameObject[] Elim;
-    public GameObject[] gone;
-    public bool Erased()
-    {
-
-        //checks to see if all squares are gone
-        //look for objects in array
-        for (int i = 0; i < gone.Length; i++)
-        {
-            //check if object is in scene 
-            if (gone[i] != null)
-            {
-                return false;
-
-
-            }
-
-        }
-
-        return true;
-
-      
-    }
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -89,24 +68,8 @@ public class RemovalScript : MonoBehaviour
 
             }
         }
-        if (collision.gameObject.name == "worm")
-        {
-            Elim = GameObject.FindGameObjectsWithTag("Round5");
-            foreach (GameObject elim in Elim) //destroy each varible in Elim
-            {
-                Destroy(elim);
-            }
-
-            Destroy(this.gameObject);
-
-
-            if (Erased())
-            {
-                SceneManager.LoadScene("End");
-
-            }
-
-        }
+        
+        
 
 
 
